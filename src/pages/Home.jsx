f3d215256aa142a132e35f8660be6ae0e4e698ae
Jarrod Bogard <jarrodbogard@gmail.com>
2024-01-26 19:43:01 -0500

    chore
    create cards to display articles on homepage in a grid pattern with some basic styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,33 +9,91 @@ const HomePage = () => {
     navigate("/articles?query=" + selection);
   };
 
+  // const displayData = (
+  //   <ul className="row list-group" style={{ width: "18rem" }}>
+  //     {articles.map((article) => (
+  //       <li
+  //         style={{ height: "20rem" }}
+  //         className="list-group-item w-100 col-4"
+  //         key={article.source.id + Math.random()}
+  //       >
+  //         <img
+  //           className="card-img-top rounded w-100"
+  //           src={article.urlToImage}
+  //           alt={article.title}
+  //         />
+
+  //         <div className="card-body w-100">
+  //           <h5 className="card-title">{article.title}</h5>
+  //           {/* <p className="card-text">{article.description}</p> */}
+  //           <a href="#" className="btn btn-primary">
+  //             Go somewhere
+  //           </a>
+  //         </div>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
+
   const displayData = (
-    <ul>
+    <div className="row">
       {articles.map((article) => (
-        <li key={article.source.id + Math.random()}>
-          <h1>{article.title}</h1>
-          <img src={article.urlToImage} alt={article.title} />
-          <p>{article.description}</p>
-        </li>
+        <div key={article.source.id + Math.random} className="col-4 g-4">
+          <div className="card">
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5>{article.title}</h5>
+              <p>{article.description}</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 
+  // const displayData = (
+  //   <ul className="row justify-content-around ">
+  //     {articles.map((article) => (
+  //       <li
+  //         className="list-group-item m-2 col-3"
+  //         key={article.source.id + Math.random()}
+  //       >
+  //         <h1>{article.title}</h1>
+  //         <img
+  //           className="img-thumbnail"
+  //           src={article.urlToImage}
+  //           alt={article.title}
+  //         />
+  //         <p>{article.description}</p>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
+
   return (
-    <div>
-      <Form action="articles">
-        <h1>Search Articles</h1>
+    <div className="container">
+      <Form className="w-50 m-auto" action="articles">
+        <h1 className="h-1 text-center text-muted">Article Search</h1>
         <Form.Control
           type="text"
           name="query"
           placeholder="Search articles here..."
         />
       </Form>
-      <div onClick={() => handleSearch("finance")}>Finance</div>
-      <div onClick={() => handleSearch("politics")}>Politics</div>
-      <div onClick={() => handleSearch("sports")}>Sports</div>
-      <div onClick={() => handleSearch("health")}>Health</div>
-      <div onClick={() => handleSearch("technology")}>Technology</div>
+      <div className="container m-2 d-flex justify-content-around">
+        <div onClick={() => handleSearch("finance")}>Finance</div>
+        <div onClick={() => handleSearch("politics")}>Politics</div>
+        <div onClick={() => handleSearch("sports")}>Sports</div>
+        <div onClick={() => handleSearch("health")}>Health</div>
+        <div onClick={() => handleSearch("technology")}>Technology</div>
+      </div>
       {displayData}
     </div>
   );
